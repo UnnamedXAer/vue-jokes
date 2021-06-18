30976e313becd6de1b4f5dc496d0355b118caa62
UnnamedXAer <UnnamedXAer@gmail.com>
2021-06-18 12:44:09 +0200

    add accepting joke functionality

--- api.js
+++ api.js
@@ -1,15 +1,23 @@
 function fetchJokes() {
 	if (Vue.config.devtools) {
-		return Promise.resolve([
-			{
-				id: 1231,
-				joke: 'Chuck Norris does not have to answer the phone. His beard picks up the incoming electrical impulses and translates them into audible sound.'
-			},
-			{
-				id: 1232,
-				joke: 'Chuck Norris was the original sculptor of Mount Rushmore. He completed the entire project using only a bottle opener and a drywall trowel.'
-			}
-		]);
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve([
+					{
+						id: 1231,
+						joke:
+							'Chuck Norris does not have to answer the phone. His beard picks up the incoming electrical impulses and translates them into audible sound.\n' +
+							new Date().toISOString()
+					},
+					{
+						id: 1232,
+						joke:
+							'Chuck Norris was the original sculptor of Mount Rushmore. He completed the entire project using only a bottle opener and a drywall trowel.\n' +
+							new Date().toISOString()
+					}
+				]);
+			}, 20);
+		});
 	}
 
 	return fetch('http://api.icndb.com/jokes/random/2')
--- main.js
+++ main.js
@@ -6,45 +6,108 @@ var alertPanel = Vue.component('alert-panel', {
 	}
 });
 
+var spinner = Vue.component('loading-indicator', {
+	template: '#tmpl-loading-indicator'
+});
+
 var svgArrowUp = Vue.component('svg-arrow-up', {
 	template: '#tmpl-svg-arrow-up'
 });
 
-var JokesActionsBtn = {
-	template: '#tmpl-jokes-actions-btn'
-};
-
-var JokesActions = {
-	template: '#tmpl-jokes-actions',
-	components: {
-		'actions-btn': JokesActionsBtn
+var JokeActionBtn = {
+	template: '#tmpl-joke-action-btn',
+	props: {
+		disabled: Boolean
 	}
 };
 
 var JokeComponent = {
 	props: {
 		joke: { type: Object, required: true },
-		idx: Number
+		accepted: Boolean
 	},
-	template: `#tmpl-joke`
+	template: '#tmpl-joke',
+	watch:{
+		accepted(val){
+			console.log("accepted", val, arguments);
+		}
+	}
 };
 
 var JokesComponent = {
 	components: {
-		'joke-card': JokeComponent,
-		'joke-actions': JokesActions
+		'action-btn': JokeActionBtn,
+		'joke-card': JokeComponent
 	},
 	data() {
 		return {
 			jokes: [],
-			fetchError: null
+			nextJokes: null,
+			loading: false,
+			fetchError: null,
+			acceptedJokeIdx: -1,
+			fetchingNext: false
 		};
+	},
+	computed: {
+		actionsDisabled() {
+			const disabled =
+				this.loading ||
+				this.fetchError !== null ||
+				this.fetchingNext ||
+				this.jokes.length < 2 ||
+				this.acceptedJokeIdx !== -1;
+
+			return disabled;
+		}
 	},
 	template: '#tmpl-jokes',
 	mounted() {
+		this.loading = true;
 		fetchJokes()
 			.then((jokes) => (this.jokes = jokes))
-			.catch((err) => (this.fetchError = err));
+			.catch((err) => (this.fetchError = err))
+			.finally(() => {
+				this.loading = false;
+			});
+	},
+	methods: {
+		acceptJoke(idx) {
+			if (this.actionsDisabled) {
+				return;
+			}
+			console.log('joke accepted: ', idx);
+			this.acceptedJokeIdx = idx;
+			Promise.all([this.acceptAnimationTimeout(), this.getNextJokes()])
+				.then((results) => {
+					const nextJokes = results[1];
+					if (!Array.isArray(nextJokes) || nextJokes.length < 2) {
+						throw new Error('Sorry, could not fetch next jokes 😓');
+					}
+					this.jokes = [...nextJokes];
+					this.acceptedJokeIdx = -1;
+				})
+				.catch((err) => {
+					this.fetchError = err;
+				})
+				.finally(() => {
+					this.fetchingNext = false;
+				});
+		},
+
+		getNextJokes() {
+			this.fetchingNext = true;
+			return fetchJokes();
+		},
+
+		acceptAnimationTimeout() {
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					this.acceptedJokeIdx = -1;
+					resolve();
+				}, 500);
+			});
+		}
 	}
 };
 
