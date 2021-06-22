4c6271a01e979ccb6a0d0f55dfe3ea9a06940006
UnnamedXAer <UnnamedXAer@gmail.com>
2021-06-22 14:32:08 +0200

    use https for api calls

--- api.js
+++ api.js
@@ -1,26 +1,26 @@
 function fetchJokes() {
-	if (window.forceFetch !== true && Vue.config.devtools) {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				resolve([
-					{
-						id: 1231,
-						joke:
-							'Chuck Norris does not have to answer the phone. His beard picks up the incoming electrical impulses and translates them into audible sound.\n' +
-							new Date().toISOString()
-					},
-					{
-						id: 1232,
-						joke:
-							'Chuck Norris was the original sculptor of Mount Rushmore. He completed the entire project using only a bottle opener and a drywall trowel.\n' +
-							new Date().toISOString()
-					}
-				]);
-			}, 20);
-		});
-	}
+	// if (window.forceFetch !== true && Vue.config.devtools) {
+	// 	return new Promise((resolve) => {
+	// 		setTimeout(() => {
+	// 			resolve([
+	// 				{
+	// 					id: 1231,
+	// 					joke:
+	// 						'Chuck Norris does not have to answer the phone. His beard picks up the incoming electrical impulses and translates them into audible sound.\n' +
+	// 						new Date().toISOString()
+	// 				},
+	// 				{
+	// 					id: 1232,
+	// 					joke:
+	// 						'Chuck Norris was the original sculptor of Mount Rushmore. He completed the entire project using only a bottle opener and a drywall trowel.\n' +
+	// 						new Date().toISOString()
+	// 				}
+	// 			]);
+	// 		}, 20);
+	// 	});
+	// }
 
-	return fetch('http://api.icndb.com/jokes/random/2')
+	return fetch('https://api.icndb.com/jokes/random/2')
 		.then((res) => res.json())
 		.then((json) => {
 			if (json.type !== 'success') {
